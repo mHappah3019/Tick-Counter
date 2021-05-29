d7ca02294a2c218b0eb42d661394ad7bd0736f81
Mirko <mkcampo2000@gmail.com>
2021-05-29 18:40:35 +0200

    added new file-to add comments

--- csv-wip.py
+++ csv-wip.py
@@ -1,5 +1,14 @@
-dates = [] # contiene 2 date
-def save_date():
+from datetime import date
+from collections import deque
+
+import csv
+import os
+
+os.chdir("C:/Users/mkcam/Desktop/Tick Counter/Tick-Counter")
+
+dates = deque(maxlen=3) # contiene 2/3 date
+
+def save_date(today, dates):
     # prende in input un array con dates[0] == "ieri l'altro"
     # e dates[1] == "ieri"
 
@@ -7,11 +16,32 @@ def save_date():
     # e dates[1] == "oggi"
 
     #appendo (ho 3 elementi), e cancello la testa
-    pass
+
+    dates.append(today)
+    dates.popleft()
+    
 
 def check_count_reset(dates):
-    # da chiamare ad ogni avvio dell'applicazione
-    #current_date =
-    #if (current_date != dates[1]):
+    # TODO: da chiamare ad ogni avvio dell'applicazione
+    current_date = date.today()
+    if (current_date != dates[1]):
+        save_date(current_date, dates)
+        count_reset()
 
-    #else:
+def count_reset():
+
+    with open("tick-instances.csv", "r") as file:
+        csv_file = csv.reader(file)
+        matrix = list(csv_file) #stores data locally in the form of a matrix where every row represents one single instance and the columns represent different parameters
+                                #NB. numbers are converted into string values
+        print(matrix)
+        print(len(matrix))
+        for i in range(len(matrix) -1):
+                matrix[i+1][2] = 0 
+
+    with open("tick-instances1.csv", "w", newline="") as file1:
+        csv_file1 = csv.writer(file1)
+        csv_file1.writerows(matrix)
+
+count_reset()
+    
