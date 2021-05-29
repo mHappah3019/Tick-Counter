d7453f84ff777edae44e3b1404e3a634e6bbeabf
Mirko <mkcampo2000@gmail.com>
2021-05-29 08:31:29 +0200

    working on time recursive functions

---
+++ csv-wip.py
@@ -0,0 +1,17 @@
+dates = [] # contiene 2 date
+def save_date():
+    # prende in input un array con dates[0] == "ieri l'altro"
+    # e dates[1] == "ieri"
+
+    # rende come output lo stesso array con dates[0] = "ieri"
+    # e dates[1] == "oggi"
+
+    #appendo (ho 3 elementi), e cancello la testa
+    pass
+
+def check_count_reset(dates):
+    # da chiamare ad ogni avvio dell'applicazione
+    #current_date =
+    #if (current_date != dates[1]):
+
+    #else:
