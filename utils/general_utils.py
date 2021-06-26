a6e5870f413338dd365fbbe55deb154f3de5bca4
Mirko <mkcampo2000@gmail.com>
2021-06-26 08:03:52 +0200

    fixing absolute path finder

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 
 def skip_last(iterator):  #TODO: understand this
@@ -16,4 +17,16 @@ def find_abs_path(file):
     return csv_path
 
 
-print(find_abs_path("dailies.csv"))
+def find_data_abs_path1(file):         #TODO: fix
+    relative_path = f"./Tick-Counter/data/{file}"
+    full_path = Path.cwd() / relative_path
+    print(Path.cwd())
+    print(full_path)
+    fullpath = Path(full_path)
+    return full_path
+
+if __name__ == "__main__":
+    print(find_abs_path("dailies.csv"))
+    p = Path(cwd = os.path.abspath(os.path.dirname(__file__)))
+    #print(Path.cwd() / "Tick-Counter" / "data")
+    print(find_data_abs_path1("dailies"))
